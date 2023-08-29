a9a720bde8bac3a589d6690a3e84aff59c8ba09d
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-08-29 10:10:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+const func=()=>{
+
+	return new Promise(resolve => {
+		setTimeout(()=>{
+
+			resolve('Hello, world!');
+		},1000)
+		
+	})
+}
+
+func().then(data=>document.getElementById('output').textContent=data)
